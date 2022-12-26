5caab7382f0e96bff0f7800dd278c92f68562c3e
JamalHarb <jamal.s.harb@hotmail.com>
2022-12-26 21:18:02 +0200

    sorting authors alphabetically in Authors assignment

--- FullStackMERN/Authors/client/src/components/AuthorList.js
+++ FullStackMERN/Authors/client/src/components/AuthorList.js
@@ -9,20 +9,20 @@ const AuthorList = (props) => {
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/authors')
-            .then(res => setAuthors(res.data.authors))
+            .then(res => setAuthors(res.data.authors)) // diong the sort here didn't work, so I put it in the render
             .catch(err => console.log(err));
-    });
+    }, []);
 
     const deleteOneAuthor = id => {
         axios.delete('http://localhost:8000/api/authors/' + id)
-            .then(res => authors.filter(author => author._id !== id))
+            .then(res => setAuthors(authors.filter(author => author._id !== id)))
             .catch(err => console.log(err));
     }
 
     return (
         <div>
             <Link to={'/authors/new'}>Add an author</Link>
-            {authors.map((author, idx) => {
+            {authors.sort((a, b) => a.name.localeCompare(b.name)).map((author, idx) => {
                 return <p key={idx}>
                     {author.name}
                     <button onClick={() => navigate('/authors/edit/' + author._id)}>Edit</button>
